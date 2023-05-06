35f8d6875b394e4967af880b5090a9baf766b782
Patrick Shea <shea.patrick.dev@gmail.com>
2023-05-06 18:48:07 -0400

    Changes

--- js/popover.js
+++ js/popover.js
@@ -1,11 +1,15 @@
 
-  var popoverTriggerList = [].slice.call(
-    document.querySelectorAll(
-      '[data-bs-toggle="popover"]'
-    )
+var popoverTriggerList = [].slice.call(
+  document.querySelectorAll(
+    '[data-bs-toggle="popover"]'
   )
-  var popoverList = popoverTriggerList.map(
-    function (popoverTriggerEl) {
-      return new bootstrap.Popover(popoverTriggerEl)
-    }
-  )
+)
+var popoverList = popoverTriggerList.map(
+  function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  }
+)
+
+$(function () {
+$("[data-toggle=popover]").popover({html:true})//add html as true to use html 
+});
